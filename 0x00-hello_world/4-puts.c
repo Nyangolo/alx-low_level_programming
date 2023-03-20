13f0e357cabeb8352842648fbf8cd6fbe68ecae3
Nyangolo <nyangoloc@gmail.com>
2023-03-20 06:20:42 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@ int main(void)
 {
 	/*string initialisation*/
 char Mystr[] = "\"Programming is like building a multilingual puzzle";
-puts(Mystr);/*writing the string to stdout*/  
+puts(Mystr);/*writing the string to stdout*/
 return (0);
 }
