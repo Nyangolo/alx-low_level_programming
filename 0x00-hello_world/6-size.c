e0de8cc7f5871b7f5d8ebd9dacccc1066935ab95
Nyangolo <nyangoloc@gmail.com>
2023-03-20 08:00:52 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,16 +10,16 @@ int main(void)
 {
 char charType;
 int integerType;
-long longtype;
-long int longinttype;
-long long int longlongtype;
+/*long longType;*/
+long int longintType;
+long long int longlongType;
 float floatType;
 	/* Sizeof operator is used to evaluate the size of a variable*/
-printf("Size of char: %ld byte(s)\n", sizeof(charType));
-printf("Size of int: %ld byte(s)\n", sizeof(integerType));
-printf("Size of long: %ld byte(s)\n", sizeof(longtype));
-printf("Size of long int: %ld byte(s)\n", sizeof(longinttype));
-printf("Size of long long int: %ld byte(s)\n", sizeof(longlongtype));
-printf("Size of float: %ld byte(s)\n", sizeof(floatType));
+printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
+/*printf("Size of a long: %ld byte(s)\n", sizeof(longType));*/
+printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 return (0);
 }
