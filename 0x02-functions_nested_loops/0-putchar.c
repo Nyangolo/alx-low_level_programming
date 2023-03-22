71640af716f1300f7853c930a16a06f4375c9c08
Nyangolo <nyangoloc@gmail.com>
2023-03-22 03:18:20 -0700

    nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
-#include<stdio.h>
+#include <stdio.h>
+#include "main.h"
+
 /**
-* main - Entry point
-*
-* Description: 'the program's description'
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point.
+ * Description: write a program that prints "_putchar" followed by a new line.
+ * Return: Always 0 (Success.)
+ */
 int main(void)
 {
-        /*string initialisation*/
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
