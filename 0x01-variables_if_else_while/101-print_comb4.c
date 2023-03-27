70f54c76e9ef8281bd574788625ae94773f1fb29
Nyangolo <nyangoloc@gmail.com>
2023-03-27 07:12:38 -0700

    changes

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - Entry point
+ * main - Prints numbers between 01 to 89 with commas and space
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 for success
  */
 int main(void)
 {
-	int d;
-
-	for (d = 0; d < 90; d++)
+int i;
+	for (i = 48; i < 56; i++)
 	{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-		}
-		if (d != 89)
+		int j;
+
+		for (j = i + 1; j < 57; j++)
 		{
-			putchar(',');
-			putchar(' ');
+			int k;
+
+			for (k = j + 1; k < 58; k++)
+			{
+
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i == 55 && j == 56 && k == 57)
+					continue;
+				putchar(44);
+				putchar(32);
+			}
 		}
+
+	}
 	putchar('\n');
 	return (0);
 }
