cc8fcdbcaa43444d14eb15bc00005fecf7b3ff15
Nyangolo <nyangoloc@gmail.com>
2023-04-05 08:10:37 -0700

    recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,7 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -8,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
-int _putchar(char c);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
 #endif
