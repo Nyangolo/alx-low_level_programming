573f02aab12908abf55896b9a3b3f1fa5dfe6c7a
Nyangolo <nyangoloc@gmail.com>
2023-04-18 14:35:24 -0700

    argc argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints out all of the arguments passed to it.
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	argv[0] = 0;
+	return (0);
+}
