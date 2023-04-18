d79a21b9e16d07d92e95325e68ac791e0dd15dba
Nyangolo <nyangoloc@gmail.com>
2023-04-18 14:31:28 -0700

    argc argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * The program prints its own name.
+ * The main program prints its own name.
  * @argc: argument count
  * @argv: argument vectors
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	argc--;/*argument count*/
+	argc--; /*argument count*/
 	printf("%s\n", argv[0]);
 	return (0);
 }
