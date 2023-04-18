a0e6082d583ef76ca6d8f6bb4c6925cea5d3cdd3
Nyangolo <nyangoloc@gmail.com>
2023-04-18 14:17:06 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  program prints its own name.
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	argc--;
+
+	printf("%s\n", argv[0]);
+	
+	return (0);
+}
