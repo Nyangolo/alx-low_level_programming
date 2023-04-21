5596f8cb5124f13d76b2680c12580f6cbdf64eb9
Nyangolo <nyangoloc@gmail.com>
2023-04-21 12:17:55 -0700

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,7 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 	if (n == 0)
 		return (0);
 	va_start(ap, n);
-	for (i = result = 0; i < n; i++)
+	for (i = result  = 0; i < n; i++)
 	{
 		result += va_arg(ap, int);
 	}
