7450bd6969d490064d5bcf51bc180747a3fa9cc3
Nyangolo <nyangoloc@gmail.com>
2023-04-21 01:54:25 -0700

    function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,27 @@
-#include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
-
+#include "3-calc.h"
 /**
- * get_op_func - ...
- * @s: ...
- *
- * Return: ...
- */
+  * get_op_func - decides which math to use based on the operator
+  * @s: pointer to string
+  * Return: result of the operation
+  */
 int (*get_op_func(char *s))(int, int)
 {
+	int i;
 	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div }
-	{ "%", op_mod },
-	{ NULL, NULL }
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
 	};
-	int i = 0;
-
+	i = 0;
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
+		if (*ops[i].op == *s && !(*(s + 1)))
+			return (ops[i].f);
 		i++;
 	}
-
-	return (0);
+	return (NULL);
 }
